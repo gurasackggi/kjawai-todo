ad6536c0594fc7f1bb02530942358a3e43acda32
Dp.k <[gurasackggi@naver.com]>
2018-11-05 17:47:30 +0900

    Updates

---
+++ precache-manifest.80ceaca2967dc4abe23a84ad132b2658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "60df9cd5b95b79f1f771",
+    "url": "/todo-app/static/js/main.60df9cd5.chunk.js"
+  },
+  {
+    "revision": "c0192f5fb0f66adb0721",
+    "url": "/todo-app/static/js/1.c0192f5f.chunk.js"
+  },
+  {
+    "revision": "60df9cd5b95b79f1f771",
+    "url": "/todo-app/static/css/main.d036850b.chunk.css"
+  },
+  {
+    "revision": "244e9a740f4b8a5a456c57b007c6ae15",
+    "url": "/todo-app/index.html"
+  }
+];
